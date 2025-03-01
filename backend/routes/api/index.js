08d2f114bd0888c8dfc9b929eb8a904b78353580
rmdelapaz <raydelapaz@gmail.com>
2025-03-01 14:25:37 -0800

    phase 1 done

---
+++ backend/routes/api/index.js
@@ -0,0 +1,11 @@
+// backend/routes/api/index.js
+const router = require('express').Router();
+
+// Test route to check if API router is working
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+
+module.exports = router;
